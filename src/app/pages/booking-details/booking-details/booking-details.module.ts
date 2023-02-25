a9a9a55650a7ed16f6c1d20a272c128e864be912
Rohan-ulti <rohan@izenbridge.com>
2023-02-25 15:51:34 +0530

    booking details

--- src/app/pages/booking-details/booking-details/booking-details.module.ts
+++ src/app/pages/booking-details/booking-details/booking-details.module.ts
@@ -7,14 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { BookingDetailsPageRoutingModule } from './booking-details-routing.module';
 
 import { BookingDetailsPage } from './booking-details.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    BookingDetailsPageRoutingModule
-  ],
+    BookingDetailsPageRoutingModule,
+    ComponentsModule  ],
   declarations: [BookingDetailsPage]
 })
 export class BookingDetailsPageModule {}
